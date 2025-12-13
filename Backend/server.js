0f65e7785552c584c5ae7408bef26b6ebe3ac720
isaiah973 <adeolaisaiah01@gmail.com>
2025-12-13 16:41:46 +0100

    fixing...

--- Backend/server.js
+++ Backend/server.js
@@ -17,6 +17,7 @@ const cartRoutes = require("./routes/cartRoutes");
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5175",
   "https://fullstack-ecommerce-production-bb8b.up.railway.app/",
 ];
 // const wishlistRoutes = require("./routes/wishlistRoutes");
