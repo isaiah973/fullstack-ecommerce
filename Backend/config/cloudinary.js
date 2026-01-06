457835faca473e753ffcde1555c6cabe2a9864d5
isaiah973 <adeolaisaiah01@gmail.com>
2026-01-06 17:54:52 +0100

    cloudinary

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -1,5 +1,5 @@
-const cloudinary = require("cloudinary").v2;
-
+// const cloudinary = require("cloudinary").v2;
+const cloudinary = require("@cloudinary/url-gen");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
